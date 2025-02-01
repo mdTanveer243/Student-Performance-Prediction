2f47d96c6de4cf523ff91ab8720ee3762bcef902
Md Tanveer <tanveersiddiqui243@gmail.com>
2025-02-01 18:28:12 +0530

    Project structure

--- setup.py
+++ setup.py
@@ -1,27 +1,20 @@
-from setuptools import find_packages , setup 
-from typing import List 
+from setuptools import find_packages, setup
+from typing import List
 
-
-def get_requirements(file_path:str) -> List[str]:
-
-    """This function will return the list of requirements"""
-
-
-    requirements=[]
+def get_requirements(file_path: str) -> List[str]:
+    """This function will return the list of requirements."""
+    requirements = []
 
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements=[req.replace('\n',"") for req in requirements]
+        requirements = [req.strip() for req in requirements]  # Using strip() to remove whitespace/newlines
 
     return requirements
 
-
 setup(
-
-    name= 'mlproject',
-    version= "0.0.1",
-    author= 'Tanveer',
-    packages = find_packages(),
-    install_requires = ['numpy','pandas']
-
+    name='mlproject',
+    version="0.0.1",
+    author='Tanveer',
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")  # Using function to read from file
 )
