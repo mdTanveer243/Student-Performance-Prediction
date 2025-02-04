b06d3f29c846d09c9870ecf8560568fb2462967d
Md Tanveer <tanveersiddiqui243@gmail.com>
2025-02-04 16:04:59 +0530

    Logging and Exception

--- template.py
+++ template.py
@@ -1,4 +1,4 @@
-import os 
+import os  
 from pathlib import Path 
 import logging 
 
@@ -9,6 +9,10 @@ project_name = "mlproject"
 list_of_files = [
 
     f"src/{project_name}/__init__.py",
+    f"src/{project_name}/logger.py",  # Moved outside `pipelines/`
+    f"src/{project_name}/exception.py",
+    f"src/{project_name}/utils.py",
+    f"src/{project_name}/components/__init__.py",
     f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/components/data_transformation.py",
     f"src/{project_name}/components/data_trainer.py",
@@ -16,30 +20,24 @@ list_of_files = [
     f"src/{project_name}/pipelines/__init__.py",
     f"src/{project_name}/pipelines/training_pipeline.py",
     f"src/{project_name}/pipelines/prediction_pipeline.py",
-    f"src/{project_name}/pipelines/exception.py",
-    f"src/{project_name}/pipelines/logger.py",
-    f"src/{project_name}/pipelines/utils.py"
     "app.py",
     "Dockers.py",
     "requirements.txt",
     "setup.py",
-
-
 ]
 
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir , filename = os.path.split(filepath)      
-    
+    filedir, filename = os.path.split(filepath)      
 
     if filedir != "":
-        os.makedirs(filedir , exist_ok = True)
-        logging.info(f"Creating directory:{filedir} for the file {filename}")
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file {filename}")
 
-    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
-        with open(filepath , 'w') as f:
+    if not os.path.exists(filepath) or os.path.getsize(filepath) == 0:
+        with open(filepath, 'w') as f:
             pass 
-        logging.info(f"creating empty file:{filepath}")
+        logging.info(f"Creating empty file: {filepath}")
 
     else:
-        logging.info(f"{filename} is already exists")
+        logging.info(f"{filename} already exists")
